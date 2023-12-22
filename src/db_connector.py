abfd01a3b5f35c0a54b2be9dafc0842baa98d6df
Marco Sanchez <mascros12@gmail.com>
2023-12-22 10:41:55 -0600

    V0.0.1

---
+++ src/db_connector.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+def push_query(qry):
+    connect = psycopg2.connect(
+        host = '172.20.5.9',
+        user = 'american',
+        password = 'HX4d2lPH*&lhl1$d7r6>RD7',
+        database = 'commissions',
+        port = '5432'
+    )
+
+    connect.autocommit = True
+    cursor = connect.cursor()
+    cursor.execute(qry)
+
+
+def get_query(qry):
+    connect = psycopg2.connect(
+        host = '172.20.5.9',
+        user = 'american',
+        password = 'HX4d2lPH*&lhl1$d7r6>RD7',
+        database = 'commissions',
+        port = '5432'
+    )
+
+    cursor.execute(qry)
+    data = cursor.fetchall()
+    return data
